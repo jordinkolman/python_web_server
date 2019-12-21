56d1623399ab0fa9609be4d25d0f8cc226853856
Jordin Kolman <jordinkolman@gmail.com>
2019-12-21 17:12:59 -0600

    edited some routing and added favicon to website

--- server.py
+++ server.py
@@ -9,10 +9,6 @@ def hello_world():
 def about():
 	return render_template('about.html')
 
-@app.route('/blog')
-def blog():
-	return 'These are my thoughts on blogs.'
-
 @app.route('/blog/2020/dogs')
 def blog2():
 	return 'This is my dog.'
