836b147fec3143c71f0807d4f64d6e57445ff217
Jordin Kolman <jordinkolman@gmail.com>
2019-12-21 18:28:12 -0600

    changed project to portfolio website

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-	return render_template('index.html', name=username, post_id=post_id)
 
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
-@app.route('/blog/2020/dogs')
-def blog2():
-	return 'This is my dog.'
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n\n{email}, {subject}, {message}')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong. Please try again.'
