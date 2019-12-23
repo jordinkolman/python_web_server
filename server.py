5f9860374ee01b8b17afad3e35f5eedde346d17d
Jordin Kolman <jordinkolman@gmail.com>
2019-12-22 21:02:00 -0600

    made small syntax fix

--- server.py
+++ server.py
@@ -20,22 +20,25 @@ def write_to_file(data):
         message = data["message"]
         file = database.write(f'\n\n{email}, {subject}, {message}')
 
+
 def write_to_csv(data):
     with open('database.csv', mode='a', newline='') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-    	try:
-        	data = request.form.to_dict()
-        	write_to_csv(data)
-        	return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
         except:
-        	return 'did not save to database'
+            return 'did not save to database'
     else:
         return 'Something went wrong. Please try again.'
